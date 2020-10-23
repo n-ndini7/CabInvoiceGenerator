b368d2d14f6a1dcae9d8ced98818d0a7b77e5254
n-ndini7 <hp@RJs-PC>
2020-10-23 18:22:03 +0530

    [nandini] added master - welcome message

---
+++ src/main/java/CabInvoiceGenerator/CabInvoiceService.java
@@ -0,0 +1,8 @@
+package CabInvoiceGenerator;
+
+//Master branch - welcome message
+public class CabInvoiceService {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Cab Invoice Service program!");
+	}
+}
