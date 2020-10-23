b0ef0465280fd7b4810b6d052640b2f75d4d2cd9
n-ndini7 <hp@RJs-PC>
2020-10-23 21:42:30 +0530

    [nandini] added UC2 - take multiple rides

--- src/main/java/CabInvoiceGenerator/CabInvoiceService.java
+++ src/main/java/CabInvoiceGenerator/CabInvoiceService.java
@@ -1,33 +1,32 @@
 package CabInvoiceGenerator;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
-//Master branch - welcome message
+//UC1 - calculate fare
+//UC2 - refactored to take multiple rides 
 public class CabInvoiceService {
 
 	public static final double MIN_COST_PER_KM = 10;
 	public static final double COST_PER_TIME = 1;
-	public static double fare;
+	public double fare;
+	public double totalFare;
 	public static final double MIN_FARE = 5;
 
 	public double calculateFare(double dis, double time) {
-		this.fare = dis * MIN_COST_PER_KM + time * COST_PER_TIME;
+		fare = dis * MIN_COST_PER_KM + time * COST_PER_TIME;
 		if (fare < MIN_FARE) {
-			return MIN_FARE;
+			fare = MIN_FARE;
 		}
 		return fare;
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to Cab Invoice Service program!");
-		System.out.println("Enter the distance(in kms): ");
-		double distance = Double.parseDouble(sc.nextLine());
-		System.out.println("Enter the time (in minutes): ");
-		double time = Double.parseDouble(sc.nextLine());
-		CabInvoiceService cab = new CabInvoiceService();
-		double fare = cab.calculateFare(distance, time);
-		System.out.println("The fare calculated is INR " + fare);
-		System.out.println("Thanks for visiting!!");
+	public double calculateFare(ArrayList<Ride> rides) {
+		for (Ride ride : rides) {
+			totalFare += calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
 	}
+
 }
--- src/test/java/CabInvoiceGenerator/CabInvoiceServiceTest.java
+++ src/test/java/CabInvoiceGenerator/CabInvoiceServiceTest.java
@@ -2,12 +2,16 @@ package CabInvoiceGenerator;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import org.junit.Assert;
 
 public class CabInvoiceServiceTest {
 
 	@Test
-	public void givenDistanceAndTimeShoulReturntheTotalFatre() {
+	public void givenDistanceAndTimeShoulReturntheTotalFare() {
 		CabInvoiceService cs = new CabInvoiceService();
 		double result = cs.calculateFare(5.0, 2.0);
 		Assert.assertEquals(52.0, result, 0.0);
@@ -19,5 +23,17 @@ public class CabInvoiceServiceTest {
 		double result = cs.calculateFare(0.1, 1.0);
 		Assert.assertEquals(5.0, result, 0.0);
 
+	}
+
+	@Test
+	public void givenMultilpleRidesShouldGiveAggregateFare() {
+		CabInvoiceService cbs = new CabInvoiceService();
+		ArrayList<Ride> rides = new ArrayList<Ride>();
+		Ride r1 = new Ride(5.0, 2.0);
+		Ride r2 = new Ride(0.1, 1.0);
+		rides.add(r1);
+		rides.add(r2);
+		double res = cbs.calculateFare(rides);
+		Assert.assertEquals(57.0, res, 0.0);
 	}
 }
