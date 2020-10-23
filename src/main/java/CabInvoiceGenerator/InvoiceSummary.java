37ab465a82cbc9df0d9da1c393b25b7e8c011959
n-ndini7 <hp@RJs-PC>
2020-10-23 22:04:52 +0530

    [nandini] added Uc3 - enhanced invoice by adding invoice summary

---
+++ src/main/java/CabInvoiceGenerator/InvoiceSummary.java
@@ -0,0 +1,27 @@
+package CabInvoiceGenerator;
+
+public class InvoiceSummary {
+
+	private int noOfRides;
+	private double totalFare;
+	private double avgFare;
+
+	public InvoiceSummary(int noOfRides, double totalFare) {
+		this.noOfRides = noOfRides;
+		this.totalFare = totalFare;
+		this.avgFare = this.totalFare/this.noOfRides;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) return true;
+		if(obj == null || getClass() != obj.getClass()) return false;
+		InvoiceSummary that = (InvoiceSummary)obj;
+		return noOfRides == that.noOfRides &&
+				Double.compare(that.totalFare, totalFare) == 0 &&
+				Double.compare(that.avgFare, avgFare) == 0;
+	}
+
+}
+
+
